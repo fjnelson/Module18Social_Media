4b4a21a77652c161955476424b50295b6d472558
fjnelson <97076779+fjnelson@users.noreply.github.com>
2023-03-12 18:14:13 -0700

    updating schema

--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 const { Schema, model } = require('mongoose');
 
-const userSchema = new Schema(
+const user = new Schema(
   {
     username: {
       type: String,
@@ -35,10 +35,10 @@ const userSchema = new Schema(
   }
 );
 
-userSchema.virtual('friendCount').get(function () {
+user.virtual('friendCount').get(function () {
   return this.friends.length;
 });
 
-const User = model('User', userSchema);
+const User = model('User', user);
 
 module.exports = User;
